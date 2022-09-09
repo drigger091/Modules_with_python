f06580a15b9382437c9bbcd8c8c7e527d404c55a
drigger091 <42235258+drigger091@users.noreply.github.com>
2022-09-09 20:58:34 +0530

    Example_file
    just to check directory contents of each module

---
+++ eg.py
@@ -0,0 +1,12 @@
+#from matplotlib import pyplot
+#from mod1 import *
+#print(dir(pyplot))
+#print(dir(mod1))
+
+import sys
+a = sys.builtin_module_names
+#print(a)
+
+import mod1
+print(dir(mod1))
+
