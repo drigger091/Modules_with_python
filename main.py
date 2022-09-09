5017b319b0ce311bf2d230df8d833926ddd7d4cb
drigger091 <42235258+drigger091@users.noreply.github.com>
2022-09-09 20:51:49 +0530

    main_file
    The modules were called and functions were executed

---
+++ main.py
@@ -0,0 +1,22 @@
+
+import mod1 as md
+
+result = md.add(10,15)
+print(result)
+
+result2 = md.sub(100000,6000)
+print(result2)
+
+result3 = md.mul(23456 ,8970)
+print(result3)
+
+result4 =md.sub(789,23)
+print(result4)
+
+result4 = md.fact(5)
+print(result4)
+
+result5 = md.pal('189')
+print(result5)
+
+
